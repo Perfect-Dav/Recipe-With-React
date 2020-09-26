580a25381293aa7a03366f6d4114b76cb781fc77
Perfect-Dav <perfectadewale31@gmail.com>
2020-09-26 14:28:44 +0100

    Build files

---
+++ build/precache-manifest.832789264fbcfa9e05a1720cdc8316a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2ba0363074db5b1f0171a3273ad88b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86ed9124ade8b298a1b0",
+    "url": "/static/css/main.17d9d11e.chunk.css"
+  },
+  {
+    "revision": "5c4b4b63cd153225f15b",
+    "url": "/static/js/2.590a2232.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.590a2232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ed9124ade8b298a1b0",
+    "url": "/static/js/main.2575dd4a.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
